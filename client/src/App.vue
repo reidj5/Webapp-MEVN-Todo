13df6e927f3ffdd6e844e2c74f0ec6001b97909c
Jamie <reid@sionnachsoftware.com>
2021-07-17 13:16:45 -0400

    Enter now submits form, as well as clicking on add button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="box">
-    <h1 class="subtitle has-text-centered" id="title">To Do List</h1>
+      <h1 class="subtitle has-text-centered" id="title">To Do List</h1>
     </div>
     <div class="box">
       <hr />
@@ -12,11 +12,12 @@
             v-model="description"
             type="text"
             placeholder="What needs done?"
+            @keyup.enter="addItem"
           />
         </div>
         <div class="control">
-          <a class="button is-info" @click="addItem" :disabled="!description"
-            >Add</a
+          <button class="button is-info" id="btn" @click="addItem" :disabled="!description" 
+            >Add</button
           >
         </div>
       </div>
@@ -125,19 +126,11 @@ export default {
 }
 
 #title {
-  color:white;
-  text-shadow: 0 1px 0 #ccc, 
-               0 2px 0 #c9c9c9,
-               0 3px 0 #bbb,
-               0 4px 0 #b9b9b9,
-               0 5px 0 #aaa,
-               0 6px 1px rgba(0,0,0,.1),
-               0 0 5px rgba(0,0,0,.1),
-               0 1px 3px rgba(0,0,0,.3),
-               0 3px 5px rgba(0,0,0,.2),
-               0 5px 10px rgba(0,0,0,.25),
-               0 10px 10px rgba(0,0,0,.2),
-               0 20px 20px rgba(0,0,0,.15);
-
+  color: white;
+  text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9,
+    0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
+    0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
+    0 5px 10px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.2),
+    0 20px 20px rgba(0, 0, 0, 0.15);
 }
 </style>
