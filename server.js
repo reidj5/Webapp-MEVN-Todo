4dc892b7b1b0698f30211cc96a65971521d03097
Jamie <reid@sionnachsoftware.com>
2021-07-16 23:00:45 -0400

    Todo Server and Client

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const { PORT, mongoUri } = require('./config')
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const bucketListItemRoutes = require('./routes/api/bucketListItems')
+const path = require('path')
+
+
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+
+mongoose
+    .connect(mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
+app.use('/api/bucketListItems', bucketListItemRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
